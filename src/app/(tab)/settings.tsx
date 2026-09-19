7faeea87b690adf2a1a9cc883ee1a84055ba4dfb
TienDao <daoduytien2608@gmail.com>
2026-09-19 15:19:03 +0700

    feat: project setup, nativewind, routing , tab navigation

--- src/app/(tab)/settings.tsx
+++ src/app/(tab)/settings.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from "react-native";
-
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 const Settings = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
